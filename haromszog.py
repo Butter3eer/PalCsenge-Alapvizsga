eeb2b680b5df36f13e23f6f71f4b08b375208ceb
Csenge Pál <butte@PumpkinPC.(none)>
2022-10-06 15:53:37 +0200

    initial commit

--- haromszog.py
+++ haromszog.py
@@ -1,6 +1,5 @@
 def haromszog() :
     oldal = int(input("Kérem a háromszög oldalának a hosszát: "))
     magassag = int(input("Kérem a háromszög magasságát: "))
-    terulet = (magassag * oldal) / 2
-    print(f"A háromszög területe: {terulet}")
+    print(f"A háromszög területe: " , (oldal * magassag / 2))
 haromszog()
